fcc84d17763f21fe05bc376fe71a95c42e2db142
smomtahe <85276162+smomtahe@users.noreply.github.com>
2023-07-20 07:49:24 -0700

    Create Deep-Sentiment-Analysis.py

---
+++ Deep-Sentiment-Analysis.py
@@ -0,0 +1,51 @@
+# Import libraries
+import numpy as np
+import pandas as pd
+import tensorflow as tf
+from tensorflow.keras.preprocessing.text import Tokenizer
+from tensorflow.keras.preprocessing.sequence import pad_sequences
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Embedding, LSTM, Dense, Dropout
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score, classification_report
+
+# Load and preprocess the data
+data = pd.read_csv('movie_reviews.csv')
+X = data['Review']
+y = data['Sentiment']
+
+# Tokenize the text data and create word embeddings
+max_words = 5000
+tokenizer = Tokenizer(num_words=max_words)
+tokenizer.fit_on_texts(X)
+X_sequences = tokenizer.texts_to_sequences(X)
+X_padded = pad_sequences(X_sequences, maxlen=100)
+
+# Step 4: Split data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X_padded, y, test_size=0.2, random_state=42)
+
+# Build the LSTM model
+embedding_dim = 100
+model = Sequential()
+model.add(Embedding(input_dim=max_words, output_dim=embedding_dim, input_length=100))
+model.add(LSTM(128))
+model.add(Dense(64, activation='relu'))
+model.add(Dropout(0.2))
+model.add(Dense(1, activation='sigmoid'))
+
+# Compile the model
+model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+batch_size = 64
+epochs = 10
+model.fit(X_train, y_train, batch_size=batch_size, epochs=epochs, validation_data=(X_test, y_test))
+
+# Make predictions and evaluate the model
+y_pred = model.predict_classes(X_test)
+y_pred = np.squeeze(y_pred)
+
+# Calculate accuracy and print classification report
+accuracy = accuracy_score(y_test, y_pred)
+print(f"Accuracy: {accuracy}")
+print(classification_report(y_test, y_pred))
